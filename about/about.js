c623d0d560f3bfd7265d96dac544c3c9a417b5a0
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-11-30 14:59:52 +0000

    1.3.0 - Implementation version check and improved About

--- about/about.js
+++ about/about.js
@@ -1,3 +1,17 @@
+// Author: Ricardo Wagemaker
+
+const path = require('path');
+const fs = require('fs');
+
+// fucntion to close the window
 document.getElementById('closeBtn').addEventListener('click', function() {
     window.close();
 });
+
+// Set version number
+const packageJsonPath = path.join(__dirname, '../package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+// Retrieve the version from package.json
+var version = packageJson.version;
+document.getElementById('version').textContent = version;
