c23e7d52dd7bda66bcbb30fff3833c2861ec0aef
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-12-06 11:54:05 +0000

    1.3.2 - Removed web-host for now until BUG #4 is fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
-import path from 'path';
-
-// Determine if HTTPS should be used based on an environment variable
-const useHttps = process.env.USE_HTTPS === 'true';
 
 // Get the port from the environment variable or default to 3000
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
@@ -12,15 +7,7 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export default defineConfig({
   plugins: [react()],
   server: {
-    port,
-    host: true,
-    ...(useHttps ? {
-      https: {
-        key: fs.readFileSync(path.resolve('/certbot/domain/privkey.pem')),
-        cert: fs.readFileSync(path.resolve('/certbot/domain/cert.pem')),
-        ca: fs.readFileSync(path.resolve('/certbot/domain/chain.pem')),
-      }
-    } : {})
+    port
   },
   build: {
     outDir: 'dist'
