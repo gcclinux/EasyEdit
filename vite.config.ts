ffe83edf3a1b945de2a393811fe30ee350cfe9dd
Ricardo Wagemaker <wagemra@gmail.com>
2025-08-11 09:37:17 +0100

    changed default ports

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // Get the port from the environment variable or default to 3000
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3024;
 
 export default defineConfig({
   plugins: [react()],
